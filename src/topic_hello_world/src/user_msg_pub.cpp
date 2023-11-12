1832a48487bb6bf6faf484ef643cd6f4cf85ef95
vistar <1055311345@qq.com>
2023-11-12 22:49:25 +0800

    code of 自定义msg

---
+++ src/topic_hello_world/src/user_msg_pub.cpp
@@ -0,0 +1,25 @@
+#include <ros/ros.h>
+#include "topic_hello_world/RobotPose.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "user_msg_pub");
+    ros::NodeHandle nh;
+
+    ros::Publisher pose_pub = nh.advertise<topic_hello_world::RobotPose>("/robot_pose", 10);
+
+    topic_hello_world::RobotPose pose;
+    pose.id = "vbot";
+    pose.x = 23.6;
+    pose.y = 12.8;
+    pose.angle = 90.0;
+
+    while(ros::ok())
+    {
+        pose_pub.publish(pose);
+        ROS_INFO("Pub robot pose(%lf, %lf, %lf)", pose.x, pose.y, pose.angle);
+        ros::Duration(1).sleep();
+    }
+
+    return 0;
+}
---
+++ src/topic_hello_world/src/user_msg_sub.cpp
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+#include "topic_hello_world/RobotPose.h"
+
+void robotPoseCallback(const topic_hello_world::RobotPose::ConstPtr &pose)
+{
+    ROS_INFO("Sub robot pose(%lf, %lf, %lf)", pose->x, pose->y, pose->angle);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "user_msg_sub");
+    ros::NodeHandle nh;
+    ros::Subscriber pose_sub = nh.subscribe<topic_hello_world::RobotPose>("/robot_pose", 10, robotPoseCallback);
+
+    ros::spin();
+
+    return 0;
+}
