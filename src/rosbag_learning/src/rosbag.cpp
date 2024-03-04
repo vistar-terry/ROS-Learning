69a56fd05ae02631a22748e188dac9a53f2d5a06
vistar <1055311345@qq.com>
2024-03-04 22:45:23 +0800

    code of rosbag::Bag

--- src/rosbag_learning/src/rosbag.cpp
+++ src/rosbag_learning/src/rosbag.cpp
@@ -1,23 +1,29 @@
-#include "ros/ros.h"
-#include "rosbag/bag.h"
-#include "std_msgs/String.h"
+#include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <ros/package.h>
+#include <std_msgs/String.h>
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "bag_write");
+
+    std::string packagePath = ros::package::getPath("rosbag_learning");
+    std::string bagsPath = packagePath + "/bags";
+
     ros::NodeHandle nh;
     // 创建bag对象
     rosbag::Bag bag;
-    // 打开
-    bag.open("./test.bag", rosbag::BagMode::Write);
-    // 写
+    // 打开文件
+    bag.open(bagsPath+"/test.bag", rosbag::BagMode::Write);
+    // 写文件
     std_msgs::String msg;
     msg.data = "hello world";
-    bag.write("/chatter", ros::Time::now(), msg);
-    bag.write("/chatter", ros::Time::now(), msg);
-    bag.write("/chatter", ros::Time::now(), msg);
-    bag.write("/chatter", ros::Time::now(), msg);
-    // 关闭
+    // 写入4帧
+    for (size_t i = 0; i < 4; i++)
+    {
+        bag.write("/chatter", ros::Time::now(), msg);
+    }
+    // 关闭文件
     bag.close();
 
     return 0;
